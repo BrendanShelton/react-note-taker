b87c0408dc2edf24a3709a3e3bdef6f8184d81d2
Brendan Shelton <brendan.d.shelton@gmail.com>
2025-01-09 20:27:42 -0500

    separate component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './assets/heading.jsx'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,29 +15,27 @@ function App() {
   const currentYear = today.getFullYear();
 
   console.log(currentYear);
+
+  let message
+  let textColor = {
+      color: ""
+  }
+  if (today.getHours() < 12) {
+    message = "good morning"
+    textColor = {color: "red"}
+  } else if (12 < today.getHours() < 18){
+    message = "good afternoon"
+    textColor = {color: "green"}
+  } else {
+    message = "good evening"
+    textColor = {color: "blue"}
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1>h1</h1>
+    
+      <heading />
+      {/* <h1 className='heading' style={textColor}>{message}</h1> */}
       <ul>
         <li>1</li>
         <li>2</li>
---
+++ src/assets/heading.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+
+function heading(){
+    return <h1>hhvguy</h1>;
+  }
+
+export default heading;
